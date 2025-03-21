cc0abb1bace0eeaa2d1856687e698fe8737d3dfe
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2025-03-21 18:04:35 +0200

    feat: call the mode switch callback

--- src/main/java/dev/dfonline/flint/event/ModeSwitchCallback.java
+++ src/main/java/dev/dfonline/flint/event/ModeSwitchCallback.java
@@ -2,22 +2,20 @@ package dev.dfonline.flint.event;
 
 import dev.dfonline.flint.Flint;
 import dev.dfonline.flint.hypercube.Mode;
-import dev.dfonline.flint.hypercube.Plot;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 
 public interface ModeSwitchCallback {
 
     Event<ModeSwitchCallback> EVENT = EventFactory.createArrayBacked(ModeSwitchCallback.class,
-            (listeners) -> (plot, newMode, oldMode) -> {
-                Flint.getUser().setPlot(plot);
+            (listeners) -> (newMode, oldMode) -> {
                 Flint.getUser().setMode(newMode);
 
                 for (ModeSwitchCallback listener : listeners) {
-                    listener.switchMode(plot, newMode, oldMode);
+                    listener.switchMode(newMode, oldMode);
                 }
             });
 
-    void switchMode(Plot plot, Mode newMode, Mode oldMode);
+    void switchMode(Mode newMode, Mode oldMode);
 
 }
