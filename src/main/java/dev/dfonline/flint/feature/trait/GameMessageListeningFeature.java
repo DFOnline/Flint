76641dd42715a5e8ca662cad1a17ac4e3569b509
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2025-03-22 16:30:49 +0200

    style: format code

--- src/main/java/dev/dfonline/flint/feature/trait/GameMessageListeningFeature.java
+++ src/main/java/dev/dfonline/flint/feature/trait/GameMessageListeningFeature.java
@@ -1,8 +1,10 @@
 package dev.dfonline.flint.feature.trait;
 
-import dev.dfonline.flint.feature.trait.results.Result;
+import dev.dfonline.flint.util.result.Result;
 import net.minecraft.text.Text;
 
 public interface GameMessageListeningFeature extends FeatureTrait {
+
     Result onGameMessage(Text text, boolean actionbar);
+
 }
