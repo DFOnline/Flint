21ab698be37cfb8d269b73bf8f7fe922b271f7cf
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2025-03-23 19:28:28 +0200

    style: format code

--- src/main/java/dev/dfonline/flint/feature/trait/SwitchModeListeningFeature.java
+++ src/main/java/dev/dfonline/flint/feature/trait/SwitchModeListeningFeature.java
@@ -4,5 +4,7 @@ import dev.dfonline.flint.feature.core.FeatureTrait;
 import dev.dfonline.flint.hypercube.Mode;
 
 public interface SwitchModeListeningFeature extends FeatureTrait {
+
     void onSwitchMode(Mode oldMode, Mode newMode);
+
 }
