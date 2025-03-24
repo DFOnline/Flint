3b8e87e283a9ef944581e865050fc49fecf5310f
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2025-03-24 19:23:59 +0200

    docs: javadoc event results

--- src/main/java/dev/dfonline/flint/util/result/EventResult.java
+++ src/main/java/dev/dfonline/flint/util/result/EventResult.java
@@ -1,8 +1,18 @@
 package dev.dfonline.flint.util.result;
 
+/**
+ * Represents the result of an event that can either proceed or be canceled.
+ */
 public enum EventResult {
 
+    /**
+     * Continue normally, possibly letting other features to cancel it.
+     */
     PASS,
+
+    /**
+     * Cancel the event, cannot be uncanceled.
+     */
     CANCEL
 
 }
--- src/main/java/dev/dfonline/flint/util/result/ReplacementEventResult.java
+++ src/main/java/dev/dfonline/flint/util/result/ReplacementEventResult.java
@@ -1,5 +1,11 @@
 package dev.dfonline.flint.util.result;
 
+/**
+ * Represents the result of an event that can either proceed normally, be canceled,
+ * or have its value replaced with a new one.
+ *
+ * @param <T> The type of value that can be replaced
+ */
 public final class ReplacementEventResult<T> {
 
     private static final ReplacementEventResult<?> PASS = new ReplacementEventResult<>(Type.PASS);
@@ -18,31 +24,74 @@ public final class ReplacementEventResult<T> {
         this.type = Type.REPLACE;
     }
 
+    /**
+     * Gets the type of this result.
+     *
+     * @return The result type
+     */
     public Type getType() {
         return this.type;
     }
 
+    /**
+     * Gets the replacement value if this result is of type REPLACE.
+     *
+     * @return The replacement value, or null if not a REPLACE result
+     */
     public T getValue() {
         return this.newMessage;
     }
 
+    /**
+     * Creates a result indicating the event should proceed normally.
+     *
+     * @param <T> The type parameter for the result
+     * @return A result with PASS type
+     */
     @SuppressWarnings("unchecked")
     public static <T> ReplacementEventResult<T> pass() {
         return (ReplacementEventResult<T>) PASS;
     }
 
+    /**
+     * Creates a result indicating the event should be canceled.
+     *
+     * @param <T> The type parameter for the result
+     * @return A result with CANCEL type
+     */
     @SuppressWarnings("unchecked")
     public static <T> ReplacementEventResult<T> cancel() {
         return (ReplacementEventResult<T>) CANCEL;
     }
 
+    /**
+     * Creates a result indicating the event should be handled with a replacement value.
+     *
+     * @param <T>        The type parameter for the result
+     * @param newMessage The replacement value to use
+     * @return A result with REPLACE type and the given replacement value
+     */
     public static <T> ReplacementEventResult<T> replace(T newMessage) {
         return new ReplacementEventResult<>(Type.REPLACE, newMessage);
     }
 
+    /**
+     * Possible types for a replacement event result.
+     */
     public enum Type {
+        /**
+         * Continue normally, possibly letting other features to cancel it.
+         */
         PASS,
+
+        /**
+         * Cancel the event, cannot be uncanceled.
+         */
         CANCEL,
+
+        /**
+         * Replace the event value with a new one.
+         */
         REPLACE
     }
 
