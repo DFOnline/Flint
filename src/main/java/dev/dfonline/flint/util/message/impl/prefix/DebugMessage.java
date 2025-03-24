64d97a8c31b9dc4563aebfd955b60760b6092bd7
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2025-03-24 13:00:30 +0200

    feat: DebugMessage allowing any Component

---
+++ src/main/java/dev/dfonline/flint/util/message/impl/prefix/DebugMessage.java
@@ -0,0 +1,28 @@
+package dev.dfonline.flint.util.message.impl.prefix;
+
+import dev.dfonline.flint.util.PaletteColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.text.format.TextDecoration;
+
+public class DebugMessage extends PrefixedMessage {
+
+    private static final Component DEBUG_PREFIX = Component.text("»", PaletteColor.PINK_LIGHT, TextDecoration.BOLD);
+
+    public DebugMessage(Component component) {
+        super(DEBUG_PREFIX, component);
+    }
+
+    public DebugMessage(Component component, boolean actionbar) {
+        super(DEBUG_PREFIX, component, actionbar);
+    }
+
+    public DebugMessage(String key, ComponentLike... args) {
+        super(DEBUG_PREFIX, Component.translatable(key, args));
+    }
+
+    public DebugMessage(String key, boolean actionbar, ComponentLike... args) {
+        super(DEBUG_PREFIX, Component.translatable(key, args), actionbar);
+    }
+
+}
