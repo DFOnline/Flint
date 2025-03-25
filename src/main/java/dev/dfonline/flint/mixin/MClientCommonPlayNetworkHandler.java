1c0ebf5578390cad1dce3b6741a0a1fad4752dc0
zBinFinn <84500718+zBinFinn@users.noreply.github.com>
2025-03-25 14:03:40 +0100

    Revert "fix: Cancelling a sent command still triggered the packet after"
    This reverts commit ea1d56e88c13783a724c32798c802832382b92be.

--- src/main/java/dev/dfonline/flint/mixin/MClientCommonPlayNetworkHandler.java
+++ src/main/java/dev/dfonline/flint/mixin/MClientCommonPlayNetworkHandler.java
@@ -29,7 +29,6 @@ public abstract class MClientCommonPlayNetworkHandler {
                 var result = ((UserCommandListeningFeature) trait).sendCommand(command);
                 if (result.getType() == ReplacementEventResult.Type.CANCEL) {
                     ci.cancel();
-                    return;
                 }
                 if (result.getType() == ReplacementEventResult.Type.REPLACE) {
                     ci.cancel();
